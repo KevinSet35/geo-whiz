ef4157fa597b63a7e91e05d76a454f942e276c18
Kevin Setayesh <kevinsetayesh7@gmail.com>
2025-06-28 13:47:10 -0400

    Cleaned up App.tsx

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -7,15 +7,15 @@ import theme from './theme/theme';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
